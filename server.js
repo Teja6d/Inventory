bb1fad1b9ebe0b73f32ad67338400cd3cd1e96d9
Teja <teja6d@gmail.com>
2021-02-07 12:23:42 +0530

    fix

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-
 const express = require('express');
 const path = require('path');
-//const app = express()
+const app = express()
 const port = process.env.PORT || 4000;
-const cors = require('cors');
-const jsonServer = require('json-server');
-//const { request } = require('http');
-//const { response } = require('express');
-// app.use(express.static(__dirname))
-//  app.use(express.static(path.join(__dirname,'build')))
-//  app.get('/',function(req,res){
-//      res.sendFile(path.join(__dirname,'build','index.html'))
-//  })
+const jsonServer = require('json-server')
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname,'build')))
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-server.use(cors())
-server.use("/db",middlewares,router)
-server.use(express.static(path.join(__dirname,'build')));
-server.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname,'build','index.html'));
-})
+server.use(middlewares)
+server.use(router)
 server.listen(port,()=>{
     console.log(`json server is running at ${port}`)
 })  
